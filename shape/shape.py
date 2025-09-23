e6e7b48370ea2a13f3f90d52d10b50f7d8191999
gunkar <gsingh@rrc.ca>
2025-09-23 09:21:02 -0500

    Adding doct Strigs to shape.py file.

--- shape/shape.py
+++ shape/shape.py
@@ -6,20 +6,31 @@ __version__ = "1.0.0"
 from abc import ABC, abstractmethod
 
 class Shape(ABC):
+   """An abstract base class for all shapes."""
+
    def __init__(self, color: str) -> None:
-    """Initialize the shape with a color."""
+    """
+        Set up the shape with a color.
+    """
     if not color.strip():
         raise ValueError("Color cannot be blank.")
     self._color = color.strip()
 
-
     def __str__(self) -> str:
+        """
+        Show the color of the shape.
+        """
         return f"The shape color is {self._color}."
 
     @abstractmethod
     def calculate_area(self) -> float:
+        """ 
+        Calculate the area of the shape.
+        """
         pass
-
+    
     @abstractmethod
     def calculate_perimeter(self) -> float:
+        """ Calculate the perimeter of the shape.
+        """
         pass
