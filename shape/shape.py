888d9a8278ad8885fc01685556ab67c084568bef
gunkar <gsingh@rrc.ca>
2025-09-23 08:54:18 -0500

    Add Shape class with color, validation, and abstract methods.

--- shape/shape.py
+++ shape/shape.py
@@ -1,4 +1,25 @@
 """This module defines the Shape class."""
 
-__author__ = ""
-__version__ = ""
+__author__ = "Gunkar singh"
+__version__ = "1.0.0"
+
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+   def __init__(self, color: str) -> None:
+    """Initialize the shape with a color."""
+    if not color.strip():
+        raise ValueError("Color cannot be blank.")
+    self._color = color.strip()
+
+
+    def __str__(self) -> str:
+        return f"The shape color is {self._color}."
+
+    @abstractmethod
+    def calculate_area(self) -> float:
+        pass
+
+    @abstractmethod
+    def calculate_perimeter(self) -> float:
+        pass
